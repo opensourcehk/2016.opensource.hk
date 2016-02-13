9c19de0a466e7dc98762fea9cabd965df131b575
Koala Yeung <koalay@gmail.com>
2016-02-14 05:10:50 +0800

    Fix client.js nav problem
    The code should only be run on front page.

--- _src/src/scripts/client.js
+++ _src/src/scripts/client.js
@@ -1,5 +1,9 @@
-$('.navbar').affix({
-  offset: {
-    top: $('.jumbotron').height()+$('.navbar').height()
-  }
-});
+
+// if on frontpage
+if (location.pathname == "/") {
+  $('.navbar').affix({
+    offset: {
+      top: $('.jumbotron').height()+$('.navbar').height()
+    }
+  });
+}
