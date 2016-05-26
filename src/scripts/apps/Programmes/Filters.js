277c9257c03ad0e81faeee2228c8e2b31c86de31
Koala Yeung <koalay@gmail.com>
2016-05-27 01:23:16 +0800

    Added FilterToggle compoment to Filters.js
    Basic toggle mechanism

--- src/scripts/apps/Programmes/Filters.js
+++ src/scripts/apps/Programmes/Filters.js
@@ -2,43 +2,84 @@ import { Component, PropTypes } from "react";
 import { connect } from 'react-redux';
 import { actions } from "./Store";
 
+
+// FilterToggle helps toggle a single filter key-value pair to on or off
+class FilterToggle extends Component {
+
+  toggle() {
+    const { onChange, filterKey, value, getStatus } = this.props;
+    const status = getStatus(filterKey, value);
+    return onChange(filterKey, value, (status === 1) ? 0 : 1);
+  }
+
+  render() {
+    const { innerText, className, filterKey, value, getStatus } = this.props;
+    const status = getStatus(filterKey, value);
+    const statusClass = (status === 1) ? "active" : "";
+    return (
+      <div className={ ["filter-toggle", className, statusClass].join(" ") }
+        onClick={ this.toggle.bind(this) }
+        data-key={ filterKey }
+        data-value={ value }
+        data-status={ status }>
+          { (innerText === null) ? value : innerText }
+      </div>
+    );
+  }
+
+}
+
+FilterToggle.defaultProps = {
+  onChange: () => {},
+  className: "",
+  innerText: null,
+  filterKey: "",
+  value: "",
+  getStatus: () => 0
+}
+
 // Filter is the UI for filtering results in the Programmes Store
 // that triggers uipdate of TimeTable
 class Filters extends Component {
 
-  handleClick(evt) {
+  handleChange(key, value, status) {
     const { store } = this.context;
-    const key = evt.target.attributes["data-key"].value;
-    const value = evt.target.attributes["data-value"].value;
-    const status = evt.target.attributes["data-status"].value;
-
-    if (status === "on") {
-      store.dispatch(actions.removeFilter(key, value));
-      evt.target.attributes["data-status"].value = "off";
-    } else {
+    if (status === 1) {
       store.dispatch(actions.addFilter(key, value));
-      evt.target.attributes["data-status"].value = "on";
+    } else {
+      store.dispatch(actions.removeFilter(key, value));
     }
+  }
+
+  filterStatus(key, value) {
+    const { filters } = this.props;
+    if (typeof filters[key] === "undefined") {
+      return 0;
+    }
+    if (filters[key].indexOf(value) !== -1) {
+      return 1;
+    }
+    return 0;
   }
 
   render() {
     // TODO: render the topics into timetable rows by their time
     return (
       <div className="filters">
-        <div className="section">
+        <div className="group">
           <h2>Level</h2>
-          <div onClick={ this.handleClick.bind(this) }
-              data-key="level" data-value="beginners" data-status="off">
-            Beginners
-          </div>
-          <div onClick={ this.handleClick.bind(this) }
-              data-key="level" data-value="intermediate" data-status="off">
-            Intermediate
-          </div>
-          <div onClick={ this.handleClick.bind(this) }
-              data-key="level" data-value="advanced" data-status="off">
-            Advanced
-          </div>
+          <FilterToggle innerText="Beginners"
+            filterKey="level" value="beginners"
+            getStatus={ this.filterStatus.bind(this) }
+            onChange={ this.handleChange.bind(this) }/>
+          <FilterToggle innerText="Intermediate"
+            filterKey="level" value="intermediate"
+            getStatus={ this.filterStatus.bind(this) }
+            onChange={ this.handleChange.bind(this) }/>
+          <FilterToggle innerText="Advanced"
+            filterKey="level" value="advanced"
+            getStatus={ this.filterStatus.bind(this) }
+            onChange={ this.handleChange.bind(this) }/>
         </div>
       </div>
     )
