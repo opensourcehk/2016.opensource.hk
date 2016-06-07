opensourcehk/2016.opensource.hk

commit 529436b41cf8ee6f39ebe3175011ecffa772931e

Improve readable, efficient of the script

src/scripts/utils/helperFuncs.js
@@ -16,18 +16,17 @@ function findSponsor(id, sponsors) {
   return null;
 }
 
+const Days = {
+  24: 'Day 1',
+  25: 'Day 2',
+  26: 'Day 3'
+};
+
 // dayName returns "Day 1", "Day 2" or "Day 3"
 // according to the date of the given timeStr
 function dayName(timeStr) {
-  switch (formatTime(timeStr, 'DD')) {
-    case "24":
-      return "Day 1";
-    case "25":
-      return "Day 2";
-    case "26":
-      return "Day 3";
-  }
-  return null;
+  let day = formatTime(timeStr, 'DD');
+  return day in Days ? Days[day] : null;
 }
 
 // formatTome parse input time string
@@ -54,7 +53,7 @@ function findTopicSummary(data, placeholder, props) {
   }
 
   // show topic summary
-  var [id, topic] = result;
+  let [id, topic] = result;
   return topicSummary(data, id);
 }
 
@@ -79,7 +78,7 @@ function topicSummary(data, id) {
 // topicURL generator
 function topicURL (type, id) {
   if (type == 'topic') {
-    return '/topics/' + id + '/';
+    return `/topics/${id}/`;
   }
 }
 
@@ -93,11 +92,7 @@ function filterBy (name, value) {
 
 // turn an object into an array
 function toArray (obj) {
-  var arr = [];
-  for ( var key in obj ) {
-      arr.push(obj[key]);
-  }
-  return arr
+  return Array.from(obj);
 }
 
 // strip tags from description text and return

gulpfile.babel.js
@@ -13,8 +13,6 @@ import moment from 'moment';
 import ls from 'ls';
 import deepFreeze from 'deep-freeze';
 
-import mocha from 'gulp-mocha';
-
 // some config files
 import webpackCfg from './configs/webpack.babel.config';
 

src/scripts/apps/Programmes/Store.js
@@ -7,16 +7,16 @@ class actions {
   // set all properties of the filters at once
   static setFilters(props) {
     return {
-      "type": "FILTER_SET",
-      props
+      props,
+      type: "FILTER_SET"
     }
   }
 
   // reset filter properties to empty object
   static resetFilters(props) {
     return {
-      "type": "FILTER_SET",
-      "props": {}
+      type: "FILTER_SET",
+      props: {}
     }
   }
 
@@ -32,18 +32,18 @@ class actions {
   // add a specific filter
   static addFilter(key, value) {
     return {
-      "type": "FILTER_ADD_PROP",
       key,
-      value
+      value,
+      type: "FILTER_ADD_PROP"
     }
   }
 
   // remove a specific filter
   static removeFilter(key, value) {
     return {
-      "type": "FILTER_REMOVE_PROP",
       key,
-      value
+      value,
+      type: "FILTER_REMOVE_PROP"
     }
   }
 
@@ -53,13 +53,15 @@ class actions {
 function filterer(filters, all) {
   return all.filter((item) => {
     for (let key in filters) {
-      var match = false
+      var match = false;
       for (let value of filters[key]) {
         if (Array.isArray(item[key])) {
+
           if (item[key].indexOf(value) !== -1) {
             match = true;
             break;
           }
+
         } else if (item[key] === value) {
           match = true;
           break;
@@ -102,7 +104,7 @@ const initialState = {
   // a shortlisted version of "all" array according to filters criteria
   // (Note: if filters is empty, show all items in "all")
   "display": []
-}
+};
 
 // apply filter add / remove actions to the store
 function reducer(state = initialState, action) {
@@ -118,7 +120,7 @@ function reducer(state = initialState, action) {
         {
           [action.key]: action.value
         }
-      )
+      );
 
       // apply filter properties and deduct the display array
       return Object.assign(

src/scripts/test/apps/Programmes/StoreTest.js
@@ -7,54 +7,54 @@ describe('actions', () => {
   it('setFilters should create an action to set all filter properties', () => {
     const props = {
       'key 1': 'value 1',
-      'key 2': 'value 2',
+      'key 2': 'value 2'
     };
     const expectedAction = {
-      type: 'FILTER_SET',
-      props
-    }
-    should(actions.setFilters(props)).deepEqual(expectedAction);
+      props,
+      type: 'FILTER_SET'
+    };
+    should(actions.setFilters(props)).eql(expectedAction).which.is.a.Object();
   });
 
   it('resetFilters should create an action to set filter properties to empty object', () => {
     const expectedAction = {
       type: 'FILTER_SET',
       props: {}
-    }
-    should(actions.resetFilters()).deepEqual(expectedAction);
+    };
+    should(actions.resetFilters()).eql(expectedAction).which.is.a.Object();
   });
 
   it('addFilter should create an action to add a filter property', () => {
     const key = 'some key';
-    const value = 'some value'
+    const value = 'some value';
     const expectedAction = {
-      type: 'FILTER_ADD_PROP',
       key,
-      value
-    }
-    should(actions.addFilter(key, value)).deepEqual(expectedAction);
+      value,
+      type: 'FILTER_ADD_PROP'
+    };
+    should(actions.addFilter(key, value)).eql(expectedAction).which.is.a.Object();
   });
 
   it('removeFilter should create an action to remove a filter property', () => {
     const key = 'some key';
-    const value = 'some value'
+    const value = 'some value';
     const expectedAction = {
-      type: 'FILTER_REMOVE_PROP',
       key,
-      value
-    }
-    should(actions.removeFilter(key, value)).deepEqual(expectedAction);
+      value,
+      type: 'FILTER_REMOVE_PROP'
+    };
+    should(actions.removeFilter(key, value)).eql(expectedAction).which.is.a.Object();
   });
 
   it('setAttribute should create an action to set an attribute', () => {
     const key = 'some key';
-    const value = 'some value'
+    const value = 'some value';
     const expectedAction = {
-      type: 'ATTR_SET',
       key,
-      value
-    }
-    should(actions.setAttribute(key, value)).deepEqual(expectedAction);
+      value,
+      type: 'ATTR_SET'
+    };
+    should(actions.setAttribute(key, value)).eql(expectedAction).which.is.a.Object();
   });
 
 });
@@ -64,7 +64,7 @@ describe('filterer', () => {
   it('should return all values if no filter is provided', () => {
 
     const all = [{"var1": "hello"}, {"var1": "world"}];
-    should(filterer({}, all)).deepEqual(all);
+    should(filterer({}, all)).eql(all).which.is.a.Object();
 
   });
 
@@ -76,24 +76,24 @@ describe('filterer', () => {
     ];
     should(
       filterer({"var1": ["hello"]}, allString)
-    ).deepEqual(
+    ).eql(
       [{"var1": "hello"}]
-    );
+    ).which.is.a.Object();
     should(
       filterer({"var1": ["world"]}, allString)
-    ).deepEqual(
+    ).eql(
       [{"var1": "world"}]
-    );
+    ).which.is.a.Object();
     should(
       filterer({"var1": ["foobar"]}, allString)
-    ).deepEqual(
+    ).eql(
       []
-    );
+    ).which.is.a.Object();
     should(
       filterer({"var2": ["hello"]}, allString)
-    ).deepEqual(
+    ).eql(
       []
-    );
+    ).which.is.a.Object();
 
     const allArray = [
       {"var1": ["hello", "foo"]},
@@ -102,41 +102,41 @@ describe('filterer', () => {
     ];
     should(
       filterer({"var1": ["hello"]}, allArray)
-    ).deepEqual([
+    ).eql([
       {"var1": ["hello", "foo"]}
-    ]);
+    ]).which.is.a.Object();
     should(
       filterer({"var1": ["world"]}, allArray)
-    ).deepEqual([
+    ).eql([
       {"var1": ["world", "bar"]}
     ]);
     should(
       filterer({"var1": ["hello", "world"]}, allArray)
-    ).deepEqual([
+    ).eql([
       {"var1": ["hello", "foo"]},
       {"var1": ["world", "bar"]}
-    ]);
+    ]).which.is.a.Object();
     should(
       filterer({"var1": ["foobar"]}, allArray)
-    ).deepEqual([
+    ).eql([
       // no result
-    ]);
+    ]).which.is.a.Object();
     should(
       filterer({"var2": ["hello"]}, allArray)
-    ).deepEqual([
+    ).eql([
       // no result
-    ]);
+    ]).which.is.a.Object();
 
   });
 
-})
+});
 
 describe('reducer', () => {
 
   it('should return initial state', () => {
     should(
       reducer(undefined, {})
-    ).deepEqual(
+    ).eql(
       {
         data: {},
         all: [],
@@ -146,7 +146,7 @@ describe('reducer', () => {
         },
         display: []
       }
-    );
+    ).which.is.a.Object();
   });
 
   it('addFilter will add filter properties and filter the display array', () => {
@@ -166,9 +166,9 @@ describe('reducer', () => {
         },
         actions.addFilter("var1", "value2")
       ).filters
-    ).deepEqual({
+    ).eql({
       "var1": ["value2"]
-    });
+    }).which.is.a.Object();
 
     should(
       reducer(
@@ -179,9 +179,9 @@ describe('reducer', () => {
         },
         actions.addFilter("var1", "value2")
       ).filters
-    ).deepEqual({
+    ).eql({
       "var1": ["value3", "value2"]
-    });
+    }).which.is.a.Object();
 
     should(
       reducer(
@@ -192,10 +192,10 @@ describe('reducer', () => {
         },
         actions.addFilter("var2", "value2")
       ).filters
-    ).deepEqual({
+    ).eql({
       "var1": ["value2"],
       "var2": ["value2"]
-    });
+    }).which.is.a.Object();
 
   });
 
@@ -219,7 +219,7 @@ describe('reducer', () => {
         },
         actions.addFilter("var2", "value2")
       ).something
-    ).equal("else");
+    ).eql("else").which.is.a.String();
 
   });
 
@@ -238,13 +238,13 @@ describe('reducer', () => {
         {
           all,
           display,
-          "filters": {"var1": ["value2"]},
+          "filters": {"var1": ["value2"]}
         },
         actions.removeFilter("var1", "value2")
       ).filters
-    ).deepEqual({
+    ).eql({
       // empty
-    });
+    }).which.is.a.Object();
 
     // remove non-existing property
     should(
@@ -252,13 +252,13 @@ describe('reducer', () => {
         {
           all,
           display,
-          "filters": {"var1": ["value2"]},
+          "filters": {"var1": ["value2"]}
         },
         actions.removeFilter("var2", "value2")
       ).filters
-    ).deepEqual({
+    ).eql({
       "var1": ["value2"]
-    });
+    }).which.is.a.Object();
 
     // handles duplicateions
     should(
@@ -272,9 +272,9 @@ describe('reducer', () => {
         },
         actions.removeFilter("var1", "value2")
       ).filters
-    ).deepEqual({
+    ).eql({
       // empty
-    });
+    }).which.is.a.Object();
 
   });
 
@@ -287,7 +287,7 @@ describe('reducer', () => {
     ];
     const filters = {
       "var1": ["value2"]
-    }
+    };
     const display = [];
     const something = "else";
 
@@ -297,12 +297,11 @@ describe('reducer', () => {
           all,
           display,
           filters,
-          something,
-          filters: {var1: ["value2"]}
+          something
         },
         actions.removeFilter("var1", "value2")
       ).something
-    ).equal("else");
+    ).equal("else").which.is.a.String();
 
   });
 
@@ -326,14 +325,14 @@ describe('reducer', () => {
         { attributes, filters, all, display },
         actions.setAttribute("hello", true)
       )
-    ).deepEqual({
+    ).eql({
+      filters,
+      all,
+      display,
       attributes: {
         foo: "bar",
         hello: true
-      },
-      filters,
-      all,
-      display
+      }
     })
   });
 
@@ -342,7 +341,7 @@ describe('reducer', () => {
     const data = {
       foo: "bar",
       something: "passby"
-    }
+    };
     const all = [
       {var1: "value1"},
       {var1: "value2"},
@@ -356,14 +355,14 @@ describe('reducer', () => {
         { data, all, display, filters },
         actions.addFilter("var1", "value2")
       ).data
-    ).deepEqual(data);
+    ).eql(data).which.is.a.Object();
 
     should(
       reducer(
         { data, all, display, filters },
         actions.removeFilter("var1", "value2")
       ).data
-    ).deepEqual(data);
+    ).eql(data).which.is.a.Object();
 
   });
 

src/scripts/utils/navBar.js
@@ -8,16 +8,15 @@ export default function () {
   let drop = $('#navbar-collapse', $navbar);
   if (!largeScreen)
     drop.removeClass('dropup');
+
   $navbar.affix({
     offset: {
       top: $('.jumbotron').height() + $navbar.height()
     }
-  });
-  $navbar.on('affixed.bs.affix', function () {
+  }).on('affixed.bs.affix', function () {
     if (largeScreen)
       drop.removeClass('dropup');
-  });
-  $navbar.on('affixed-top.bs.affix', function () {
+  }).on('affixed-top.bs.affix', function () {
     if (largeScreen)
       drop.addClass('dropup');
   });

commit d79df0b06a8ea0b5582d659bc01c4241a35fbfe5

Fix bug in helperFuncs introduced in 529436b
Fixed helperFuncs.toArray. 529436b attempts to implement it with
`Array.from()` but failed.

src/scripts/utils/helperFuncs.js
@@ -92,7 +92,11 @@ function filterBy (name, value) {
 
 // turn an object into an array
 function toArray (obj) {
-  return Array.from(obj);
+  var arr = [];
+  for ( var key in obj ) {
+      arr.push(obj[key]);
+  }
+  return arr;
 }
 
 // strip tags from description text and return

src/scripts/test/utils/helperFuncsTest.js
@@ -1,0 +1,14 @@
+import helperFuncs from '../../utils/helperFuncs.js'
+import should from "should";
+
+describe('helperFuncs.toArray', () => {
+  it('should convert all object values to an array', () => {
+    should(helperFuncs.toArray({
+      key1: "value 1",
+      key2: "value 2"
+    })).eql([
+      "value 1",
+      "value 2"
+    ]).which.is.an.Array();
+  })
+});